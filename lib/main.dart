4fdcd790ba606e78a81b5c7be4971b1278e91e45
fazeleardestani <fazeleardestani@gmail.com>
2024-02-14 19:47:38 +0330

    fix api service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:we_wed/firebase_options.dart';
 import 'package:we_wed/services/notification_service.dart';
@@ -9,16 +11,18 @@ import 'package:we_wed/routes/names.dart';
 import 'package:we_wed/routes/pages.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:flutter_native_timezone/flutter_native_timezone.dart';
-import 'package:timezone/timezone.dart' as tz;
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService().initNotification();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   tz.initializeTimeZones();
-  final String timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
-  tz.setLocalLocation(tz.getLocation(timeZoneName));
+  var initializationSettingsAndroid = const AndroidInitializationSettings('app_icon');
+  var initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  
 
   runApp(const MainApp());
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:we_wed/models/services_model.dart';
 
 class ApiService {
-  static const String apiUrl = "http://138.201.210.10/api/testapi";
+  static const String apiUrl = "http://138.201.210.8/api/testapi";
 
   static Future<List<ServiceModel>> fetchServices() async {
     final response = await http.post(Uri.parse(apiUrl));
@@ -11,7 +11,8 @@ class ApiService {
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
       final List<dynamic> servicesData = data['data'];
-      List<ServiceModel> services = servicesData.map((json) => ServiceModel.fromJson(json)).toList();
+      List<ServiceModel> services =
+          servicesData.map((json) => ServiceModel.fromJson(json)).toList();
       return services;
     } else {
       throw Exception("Failed to load services");
